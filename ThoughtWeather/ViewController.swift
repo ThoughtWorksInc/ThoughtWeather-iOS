59cb02d982da7dfbf70a83ff99ae38d73f3e9ddd
Michael Chaffee <mchaffee@thoughtworks.com>
2023-05-09 08:48:40 -0400

    Initial Commit

---
+++ ThoughtWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThoughtWeather
+//
+//  Created by Michael Chaffee on 2023-05-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
